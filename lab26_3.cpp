bad382918551fb74b3d7ca8ec4aa3984ce852b3e
auswitch <sawit_cha@cmu.ac.th>
2022-02-18 11:15:11 +0700

    lab 26 completed

--- lab26_3.cpp
+++ lab26_3.cpp
@@ -1,8 +1,30 @@
 #include<iostream>
-
+#include <set>
 using namespace std;
 
-//Write function count() here.
+int count(int data[], int idx)
+{
+	int count = 0, same = 0;
+	set<int> mySet;
+	for(int i = 0; i < idx; i++)
+	{
+		same = 0;
+		for(int j = i; j < idx; j++)
+		{
+			if(data[j] == data[i])
+			{
+				same++;
+			}
+		}
+
+		if(same == 1)
+		{
+			mySet.insert(data[i]);
+			count++;
+		}
+	}
+	return count;
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
